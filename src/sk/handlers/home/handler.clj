1722b0d8a493725d4ec9b0cbceb406ecfcb2afca
Hector Lucero <hectorqlucero@gmail.com>
2020-04-01 00:06:07 -0700

    Added exception handling to home/register handlers

--- src/sk/handlers/home/handler.clj
+++ src/sk/handlers/home/handler.clj
@@ -18,43 +18,58 @@
    FROM users
    WHERE id = ?")
 
-(defn get-main-title []
-  (let [id (get-session-id)
-        title (if (= id 1)
-                (str "<strong>User:</strong> " (:username (first (Query db [main-sql id]))))
-                "Click on the Login Menu to access site.")]
-    title))
+(defn get-main-title 
+  []
+  (try
+    (let [id (get-session-id)
+          title (if (= id 1)
+                  (str "<strong>User:</strong> " (:username (first (Query db [main-sql id]))))
+                  "Click on the Login Menu to access site.")]
+      title)
+    (catch Exception e (.getMessage e))))
 
-(defn main [request]
-  (let [title (get-main-title)
-        ok (get-session-id)
-        content [:div [:span {:style "margin-left:20px;"} (get-main-title)]] ]
-    (application title ok nil content)))
+(defn main 
+  [request]
+  (try
+    (let [title (get-main-title)
+          ok (get-session-id)
+          content [:div [:span {:style "margin-left:20px;"} (get-main-title)]] ]
+      (application title ok nil content))
+    (catch Exception e (.getMessage e))))
 ;; End Main
 
 ;; Start Login
-(defn login [_]
-  (let [title "Login"
-        ok (get-session-id)
-        content (login-view (anti-forgery-field))
-        scripts (login-script)]
-    (if-not (= (get-session-id) 0)
-      (redirect "/")
-      (application title ok scripts content))))
+(defn login 
+  [_]
+  (try
+    (let [title "Login"
+          ok (get-session-id)
+          content (login-view (anti-forgery-field))
+          scripts (login-script)]
+      (if-not (= (get-session-id) 0)
+        (redirect "/")
+        (application title ok scripts content)))
+    (catch Exception e (.getMessage e))))
 
-(defn login! [username password]
-  (let [row (first (Query db ["SELECT * FROM users WHERE username = ?" username]))
-        active (:active row)]
-    (if (= active "T")
-      (do
-        (if (crypt/compare password (:password row))
-          (do
-            (session/put! :user_id (:id row))
-            (generate-string {:url "/"}))
-          (generate-string {:error "Unable to access the site!"})))
-      (generate-string {:error "The user is inactive!"}))))
+(defn login! 
+  [username password]
+  (try
+    (let [row (first (Query db ["SELECT * FROM users WHERE username = ?" username]))
+          active (:active row)]
+      (if (= active "T")
+        (do
+          (if (crypt/compare password (:password row))
+            (do
+              (session/put! :user_id (:id row))
+              (generate-string {:url "/"}))
+            (generate-string {:error "Unable to access the site!"})))
+        (generate-string {:error "The user is inactive!"})))
+    (catch Exception e (.getMessage e))))
 ;; End login
 
-(defn logoff []
-  (session/clear!)
-  (redirect "/"))
+(defn logoff 
+  []
+  (try
+    (session/clear!)
+    (redirect "/")
+    (catch Exception e (.getMessage e))))
--- src/sk/handlers/registrar/handler.clj
+++ src/sk/handlers/registrar/handler.clj
@@ -23,105 +23,129 @@
                                     create-token]]))
 
 ;; Start registrar
-(defn registrar [request]
-  (let [title "Register Users"
-        token (anti-forgery-field)
-        ok (get-session-id)
-        js (registrar-scripts)
-        error-text "A session exists, cannot create a new user"
-        return-url "/"
-        content (registrar-view title token)]
-    (if (> ok 0)
-      (error-404 error-text return-url)
-      (application title ok js content))))
+(defn registrar 
+  [request]
+  (try
+    (let [title "Register Users"
+          token (anti-forgery-field)
+          ok (get-session-id)
+          js (registrar-scripts)
+          error-text "A session exists, cannot create a new user"
+          return-url "/"
+          content (registrar-view title token)]
+      (if (> ok 0)
+        (error-404 error-text return-url)
+        (application title ok js content)))
+    (catch Exception e (.getMessage))))
 
-(defn registrar! [{params :params}]
+(defn registrar!
   "Post user data to the users table"
-  (let [email (clojure.string/lower-case (or (:email params) "0"))
-        password (:password params)
-        params (assoc params 
-                      :level "u"
-                      :active "t"
-                      :password (crypt/encrypt password)
-                      :email email
-                      :username email)
-        postvars (assoc (build-postvars "users" params)
+  [{params :params}]
+  (try
+    (let [email (clojure.string/lower-case (or (:email params) "0"))
+          password (:password params)
+          params (assoc params 
+                        :level "u"
+                        :active "t"
+                        :password (crypt/encrypt password)
                         :email email
                         :username email)
-        result (Save db :users postvars ["username = ?" email])]
-    (if (seq result)
-      (generate-string {:url "/login"})
-      (generate-string {:error "Unable to register user!"}))))
+          postvars (assoc (build-postvars "users" params)
+                          :email email
+                          :username email)
+          result (Save db :users postvars ["username = ?" email])]
+      (if (seq result)
+        (generate-string {:url "/login"})
+        (generate-string {:error "Unable to register user!"})))
+    (catch Exception e (.getMessage e))))
 ;; End registrar
 
 ;; Start reset-password
-(defn reset-password [request]
-  (let [title "Reset Password"
-        token (anti-forgery-field)
-        ok (get-session-id)
-        error-text "A session exists, cannot create a new user"
-        return-url "/"
-        js (reset-password-scripts)
-        content (reset-password-view title token)]
-    (if (> ok 0)
-      (error-404 error-text return-url)
-      (application title ok js content))))
+(defn reset-password 
+  [request]
+  (try
+    (let [title "Reset Password"
+          token (anti-forgery-field)
+          ok (get-session-id)
+          error-text "A session exists, cannot create a new user"
+          return-url "/"
+          js (reset-password-scripts)
+          content (reset-password-view title token)]
+      (if (> ok 0)
+        (error-404 error-text return-url)
+        (application title ok js content)))
+    (catch Exception e (.getMessage e))))
 
-(defn get-username-row [username]
-  (first (Query db ["SELECT * FROM users WHERE username = ?" username])))
+(defn get-username-row 
+  [username]
+  (try
+    (first (Query db ["SELECT * FROM users WHERE username = ?" username]))
+    (catch Exception e (.getMessage e))))
 
-(defn email-body [row url]
+(defn email-body
   "Create the email body"
-  (let [nombre       (str (:firstname row) " " (:lastname row))
-        email        (:email row)
-        subject      "Reset your password"
-        content      (str "<strong>Hi</strong> " nombre ",</br></br>"
-                          "To reset your password <strong>" "<a href='" url "'>Click Here</a>" "</strong>.</br></br>"
-                          "Alternatively you can copy and paste the following link on your browsers address field:</br></br>"
-                          url "</br></br>"
-                          "This link is only good for 10 minutes.</br></br>"
-                          "If you did not request to change your password or don't want to change your password simply ignore this message.</br></br></br>"
-                          "Sincerely,</br></br>"
-                          "The administration")
-        body         {:from    (:email-user config)
-                      :to      email
-                      :subject subject
-                      :body    [{:type    "text/html;charset=utf-8"
-                                 :content content}]}]
-    body))
+  [row url]
+  (try
+    (let [nombre       (str (:firstname row) " " (:lastname row))
+          email        (:email row)
+          subject      "Reset your password"
+          content      (str "<strong>Hi</strong> " nombre ",</br></br>"
+                            "To reset your password <strong>" "<a href='" url "'>Click Here</a>" "</strong>.</br></br>"
+                            "Alternatively you can copy and paste the following link on your browsers address field:</br></br>"
+                            url "</br></br>"
+                            "This link is only good for 10 minutes.</br></br>"
+                            "If you did not request to change your password or don't want to change your password simply ignore this message.</br></br></br>"
+                            "Sincerely,</br></br>"
+                            "The administration")
+          body         {:from    (:email-user config)
+                        :to      email
+                        :subject subject
+                        :body    [{:type    "text/html;charset=utf-8"
+                                   :content content}]}]
+      body)
+    (catch Exception e (.getMessage e))))
 
-(defn reset-password! [request]
-  (let [params     (:params request)
-        username   (:email params)
-        token      (create-token username)
-        url        (get-reset-url request token)
-        row        (get-username-row username)
-        email-body (email-body row url)]
-    (if (future (send-email host email-body))
-      (generate-string {:url "/"})
-      (generate-string {:error "Unable to reset your password!"}))))
+(defn reset-password! 
+  [request]
+  (try
+    (let [params     (:params request)
+          username   (:email params)
+          token      (create-token username)
+          url        (get-reset-url request token)
+          row        (get-username-row username)
+          email-body (email-body row url)]
+      (if (future (send-email host email-body))
+        (generate-string {:url "/"})
+        (generate-string {:error "Unable to reset your password!"})))
+    (catch Exception e (.getMessage e))))
 ;; End reset-password
 
-(defn reset-jwt [token]
-  (let [title "Reset Password"
-        csrf (anti-forgery-field)
-        ok (get-session-id)
-        username (check-token token)
-        error-text "Your token is incorrect or it has expired!"
-        return-url "/"
-        js (reset-jwt-scripts)
-        content (reset-jwt-view title csrf username)]
-    (if-not (nil? username)
-      (application title ok js content)
-      (error-404 error-text return-url))))
+(defn reset-jwt 
+  [token]
+  (try
+    (let [title "Reset Password"
+          csrf (anti-forgery-field)
+          ok (get-session-id)
+          username (check-token token)
+          error-text "Your token is incorrect or it has expired!"
+          return-url "/"
+          js (reset-jwt-scripts)
+          content (reset-jwt-view title csrf username)]
+      (if-not (nil? username)
+        (application title ok js content)
+        (error-404 error-text return-url)))
+    (catch Exception e (.getMessage e))))
 
-(defn reset-jwt! [{params :params}]
-  (let [username (or (:username params) "0")
-        postvars  {:username username
-                   :password  (crypt/encrypt (:password params))}
-        result (Update db :users postvars ["username = ?" username])]
-    (if (seq result)
-      (generate-string {:url "/"})
-      (generate-string {:error "Unable to reset your password!"}))))
+(defn reset-jwt! 
+  [{params :params}]
+  (try
+    (let [username (or (:username params) "0")
+          postvars  {:username username
+                     :password  (crypt/encrypt (:password params))}
+          result (Update db :users postvars ["username = ?" username])]
+      (if (seq result)
+        (generate-string {:url "/"})
+        (generate-string {:error "Unable to reset your password!"})))
+    (catch Exception e (.getMessage e))))
 ;; End reset-password
 
