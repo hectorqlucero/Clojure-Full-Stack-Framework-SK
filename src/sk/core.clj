830ac9da77cbaf9263be3bbd6b63fb012a27e4e7
Hector Lucero Quihuis <hectorqlucero@fastmail.com>
2021-03-11 11:20:58 -0800

    Fixed handlers

--- src/sk/core.clj
+++ src/sk/core.clj
@@ -10,7 +10,7 @@
             [ring.middleware.reload :as reload]
             [ring.middleware.session :refer [wrap-session]]
             [ring.middleware.session.cookie :refer [cookie-store]]
-            [sk.models.crud :refer [config]]
+            [sk.models.crud :refer [config KEY]]
             [sk.routes :refer [open-routes]]
             [sk.proutes :refer [proutes]])
   (:gen-class))
@@ -18,7 +18,7 @@
 (defn wrap-login [hdlr]
   (fn [req]
     (try
-      (if (nil? (session/get :user_id)) (redirect "/") (hdlr req))
+      (if (nil? (session/get :user_id)) {:status 400 :body "Unable to process your request!"} (hdlr req))
       (catch Exception _
         {:status 400 :body "Unable to process your request!"}))))
 
@@ -29,33 +29,25 @@
       (catch Exception _
         {:status 400 :body "Invalid data"}))))
 
-(defroutes public-routes
-  open-routes)
-
-(defroutes protected-routes
-  proutes)
-
 (defroutes app-routes
   (route/resources "/")
   (route/files "/uploads/" {:root (:uploads config)})
+  open-routes
+  (wrap-login proutes)
   (route/not-found "Not Found"))
 
 (defn -main []
   (jetty/run-jetty
     (-> (routes
-          public-routes
-          (wrap-exception-handling public-routes)
-          (wrap-login protected-routes)
-          (wrap-exception-handling protected-routes)
-          app-routes)
+          (wrap-exception-handling app-routes))
         (handler/site)
+        (wrap-session)
+        (session/wrap-noir-session*)
         (wrap-multipart-params)
         (reload/wrap-reload)
         (wrap-defaults (-> site-defaults
                            (assoc-in [:security :anti-forgery] true)
-                           (assoc-in [:session :store] (cookie-store))
+                           (assoc-in [:session :store] (cookie-store {:key KEY}))
                            (assoc-in [:session :cookie-attrs] {:max-age 28800})
-                           (assoc-in [:session :cookie-name] "LS")))
-        (session/wrap-noir-session*)
-        (wrap-session))
+                           (assoc-in [:session :cookie-name] "LS"))))
     {:port (:port config)}))
