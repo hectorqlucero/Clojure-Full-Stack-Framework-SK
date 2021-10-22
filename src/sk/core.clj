03b99929ffbfbb1f92203cb51d78bb3779b2c3cd
Hector Lucero <hectorqlucero@gmail.com>
2021-10-22 16:37:57 -0700

    Minor change

--- src/sk/core.clj
+++ src/sk/core.clj
@@ -37,15 +37,15 @@
 
 (defn -main []
   (jetty/run-jetty
-    (-> (routes
-          (wrap-exception-handling app-routes))
-        (wrap-session)
-        (session/wrap-noir-session*)
-        (wrap-multipart-params)
-        (reload/wrap-reload)
-        (wrap-defaults (-> site-defaults
-                           (assoc-in [:security :anti-forgery] true)
-                           (assoc-in [:session :store] (cookie-store {:key KEY}))
-                           (assoc-in [:session :cookie-attrs] {:max-age 28800})
-                           (assoc-in [:session :cookie-name] "LS"))))
-    {:port (:port config)}))
+   (-> (routes
+        (wrap-exception-handling app-routes))
+       (wrap-session)
+       (session/wrap-noir-session*)
+       (wrap-multipart-params)
+       (reload/wrap-reload)
+       (wrap-defaults (-> site-defaults
+                          (assoc-in [:security :anti-forgery] true)
+                          (assoc-in [:session :store] (cookie-store {:key KEY}))
+                          (assoc-in [:session :cookie-attrs] {:max-age 28800})
+                          (assoc-in [:session :cookie-name] "LS"))))
+   {:port (:port config)}))
