6025f74415b0cd644048d60a68765f7caced72b4
Hector Lucero <hectorqlucero@gmail.com>
2022-06-04 23:05:38 -0700

    Minor fixes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sk "0.1.0"
-  :description "Pet-Daleando"
-  :url "https://github.com/hectorqlucero/pd"
+  :description "Sitio"
+  :url "https://github.com/hectorqlucero/sk"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
