bc46a26b14a6277ee59c7844e393df172333767f
Hector Lucero <hectorqlucero@gmail.com>
2022-07-27 21:03:57 -0700

    Added csv library

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
+                 [org.clojure/data.csv "1.0.1"]
                  [compojure "1.6.2" :exclusions [commons-codec]]
                  [hiccup "1.0.5"]
                  [lib-noir "0.9.9"]
