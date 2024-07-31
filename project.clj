5ba5359743cccf28501097f33b1c10f7f0fb915b
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2024-07-31 15:58:28 -0700

    Updated libraries

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/hectorqlucero/sk" ; Change me
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.csv "1.0.1"]
-                 [compojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [org.clojure/data.csv "1.1.0"]
+                 [compojure "1.7.1"]
                  [hiccup "1.0.5"]
                  [lib-noir "0.9.9"]
                  [com.draines/postal "2.0.5"]
-                 [cheshire "5.12.0"]
+                 [cheshire "5.13.0"]
                  [clj-pdf "2.6.8"]
                  [ondrs/barcode "0.1.0"]
                  [pdfkit-clj "0.1.7"]
@@ -18,10 +18,10 @@
                  [clj-time "0.15.2"]
                  [date-clj "1.0.1"]
                  [org.clojure/java.jdbc "0.7.12"]
-                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/data.codec "0.2.0"]
                  [mysql/mysql-connector-java "8.0.33"]
                  [ragtime "0.8.1"]
-                 [ring/ring-core "1.11.0"]]
+                 [ring/ring-core "1.12.2"]]
   :main ^:skip-aot sk.core
   :aot [sk.core]
   :plugins [[lein-ancient "0.7.0"]
